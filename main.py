ee6e394237b08d52094b4f4254697d8d82535733
Vikrom Narula <vikrom.nar@gmail.com>
2021-12-03 09:35:52 +0700

    Added ability to display top 20 coins be marketcap.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def fetchCoins():
     table_body = soup.find('tbody')
     rows = table_body.findAll('tr')
     top10 = rows[:10]
+    rest20 = rows[11:21]
 
     coins = []
     for row in top10:
@@ -16,8 +17,14 @@ def fetchCoins():
         name, symbol = coin[0].getText(), coin[1].getText()
         coins.append((symbol, name))
 
-    return coins
+    for row in rest20:
+        data_cell = row.findAll('td')
+        coin = data_cell[2].findAll('span')
+        name, symbol = coin[1].getText(), coin[2].getText()
+        coins.append((symbol, name))
 
+    return coins
 
 if __name__ == '__main__':
     coins = fetchCoins()
+    print(coins)
