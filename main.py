da8507e2edc91294f3a8fe001b7abf89d38430c6
Vikrom Narula <vikrom.nar@gmail.com>
2021-12-03 10:07:34 +0700

    Added ability to display crudely data

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import datetime as dt
+
+import pandas as pd
+import pandas_datareader as web
 import requests
 from bs4 import BeautifulSoup
+from dateutil.relativedelta import relativedelta
 
 def fetchCoins():
     url = 'https://coinmarketcap.com/'
@@ -33,8 +38,58 @@ def filterCoins(coins):
             lst.append(coin)
     return lst
 
+def fetchPrice(symbol='BTC-USD', gap=6, day=False):
+    end = dt.datetime.now()
+    start = end - relativedelta(months=gap)
+    if day:
+        start = end - relativedelta(days=gap)
+    return web.DataReader(symbol, 'yahoo', start, end)
+
+def getChange(current, previous):
+    if current == previous:
+        return 0
+    try:
+        return ((current - previous) / previous) * 100.0
+    except ZeroDivisionError:
+        return float('inf')
+
 if __name__ == '__main__':
     temp_coins = fetchCoins()
     coins = filterCoins(temp_coins)
 
     print(coins)
+
+    time_gaps = [1, 3, 6, 12]
+    months = ['1m', '3m', '6m', '12m']
+    for coin in coins:
+        symbol, name = coin
+        symbol_usd = f'{symbol}-USD'
+        currents, means, changes = [], [], []
+        for gap in time_gaps:
+            try:
+                pd.DataFrame(fetchPrice(symbol=symbol_usd, gap=gap))
+            except:
+                symbol_usd = f'{symbol}1-USD'
+
+            data = pd.DataFrame(fetchPrice(symbol=symbol_usd, gap=gap))
+            close = data['Close']
+
+            current = close[-1]
+            mean = close.mean()
+            change = getChange(current, mean)
+
+            if round(current, 3) < 0.001:
+                currents.append(current)
+                means.append(mean)
+                changes.append(change)
+            else:
+                currents.append(round(current, 3))
+                means.append(round(mean, 3))
+                changes.append(round(change, 3))
+
+        df = pd.DataFrame(data={
+            'Current': currents,
+            'Mean': means,
+            '% Change': changes
+        }, index=months)
+        print(df)
