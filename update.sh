9618a9012c3ffbccc4bf0d355ce0d0be83efa9eb
Vikrom Narula <vikrom.nar@gmail.com>
2022-01-16 11:33:52 +0700

    Imrpove bash script

--- update.sh
+++ update.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-rm out.txt
-
 echo "Computing..."
 start=`date +%s`
-echo "\`\`\`" >> out.txt
+echo "\`\`\`" > out.txt
 pipenv run python3 main.py >> out.txt
 echo "\`\`\`" >> out.txt
 end=`date +%s`
